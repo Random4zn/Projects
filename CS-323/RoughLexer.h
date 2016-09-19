751c9fa54cf01aa31f7c31fa4f6666b58a41bb45
Jayteare <Jayteare@users.noreply.github.com>
2016-09-18 19:26:19 -0700

    Create RoughLexer.h

---
+++ CS-323/RoughLexer.h
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <ctype.h>
+#include <cctype>
+#include <algorithm>
+using namespace std;
