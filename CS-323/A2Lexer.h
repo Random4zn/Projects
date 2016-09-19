ffc1bf2960750244207a4abf27191f34441ac998
Jayteare <Jayteare@users.noreply.github.com>
2016-09-18 19:26:52 -0700

    Create A2Lexer.h

---
+++ CS-323/A2Lexer.h
@@ -0,0 +1,14 @@
+#include "RoughLexer.h"
+
+class RuleSet_A2
+{
+	public:
+		vector<string> parseID(vector<string> parseStack);
+		vector<string> parseKey(vector<string> parseStack);
+		vector<string> parseOp(vector<string> parseStack);
+		vector<string> ruleSet_();
+
+	private:
+		std::vector<std::string> A2RuleSet;
+
+};
