d3c6d690d53a493cdc2c32d4aef5040e124a80ec
Jayteare <Jayteare@users.noreply.github.com>
2016-09-20 13:49:45 -0700

    Update A2Lexer.h

--- CS-323/A2Lexer.h
+++ CS-323/A2Lexer.h
@@ -12,3 +12,19 @@ class RuleSet_A2
 		std::vector<std::string> A2RuleSet;
 
 };
+
+class Parser_A2
+{
+	public:
+		vector<string> read_Text(string File_Name);
+		void Parser_A2Lexi(vector<string> _id, vector<string> _key, vector<string> _operation, vector<string> Text_File);
+		string token_match(vector<string> _id, vector<string> _key, vector<string> _operation, string _line, string _line_num);
+
+		string int_check(string line_);
+		string float_check(string line_);
+		void next_token();
+
+	private:
+		vector<string> Text_File;
+		int number_of_lines = 0;
+};
