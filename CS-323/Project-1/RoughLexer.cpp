dd6874856500de46c6c3279f9035bdcc4fbe9991
Jayteare <Jayteare@users.noreply.github.com>
2016-09-24 04:56:13 -0700

    Update RoughLexer.cpp

--- CS-323/Project-1/RoughLexer.cpp
+++ CS-323/Project-1/RoughLexer.cpp
@@ -59,29 +59,49 @@ int main()
 	 * Decleration segment.
      */
 	Parser_A2 parser_;
-	vector<string>File_Text_1 = parser_.read_Text("TestCase_1.txt");
+
+	cout << "-------------------------------------------------" << endl;
+	cout << "Project 1 - A2 Lexicon" << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "Authors : Can Dalgir, David Luong."  << endl;
+	cout << "Class   : CPSC-323" << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "DESCRIPTION" << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "Program information can be found"    << endl;
+	cout << "located in the ReadMe.txt file."     << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "CONTACT INFORMATION" << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "Can Dalgir e-mail  : cdalgir@csu.fullerton.edu" << endl;
+	cout << "David Luong e-mail : davidluong@csu.fullerton.edu" << endl;
+	cout << "-------------------------------------------------" << endl;
+
 
 	//FUNCTION CALL - Parser_A2Lexi
+	vector<string>File_Text_1 = parser_.read_Text("TestCase_1.txt");
 	parser_.Parser_A2Lexi(A2iD_, A2Key_, A2Op_, File_Text_1);
 	cout << "Finished parsing Testcase_1..." << endl;
 	parser_.cleanParser_();
+	cout << "Press enter to continue..." << endl;
 	cin.ignore();
 
 	vector<string>File_Text_2 = parser_.read_Text("TestCase_2.txt");
 	parser_.Parser_A2Lexi(A2iD_, A2Key_, A2Op_, File_Text_2);
 	cout << "Finished parsing Testcase_2..." << endl;
 	parser_.cleanParser_();
+	cout << "Press enter to continue..." << endl;
 	cin.ignore();
 
 	vector<string>File_Text_3 = parser_.read_Text("TestCase_3.txt");
 	parser_.Parser_A2Lexi(A2iD_, A2Key_, A2Op_, File_Text_3);
 	cout << "Finished parsing Testcase_3..." << endl;
 	parser_.cleanParser_();
+	cout << "Press enter to continue..." << endl;
 	cin.ignore();
 
     //------------------------------------------------------------------------------
 	//Halts the Visual Studio console for readability of the correct output.
-	std::cin.ignore();
 	std::cin.ignore();
 	return 0;
 }
