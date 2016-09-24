52769478b3e059ae839d8fdc7a1a3e750f1aa4ab
Jayteare <Jayteare@users.noreply.github.com>
2016-09-24 02:40:34 -0700

    Create RoughLexer.h

---
+++ CS-323/Project-1/RoughLexer.h
@@ -0,0 +1,20 @@
+/*****************************************************************
+* HEADER - C++ HEADERS
+* ----------------------------------------------------------------
+* This section is designed to hold all the user headers in the
+* current program. Additionally it will hold any additional
+* functions created to assist the main execution.
+* However most of the heavy load will be handled through the
+* class structure(s).
+******************************************************************/
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <ctype.h>
+#include <cctype>
+#include <algorithm>
+using namespace std;
